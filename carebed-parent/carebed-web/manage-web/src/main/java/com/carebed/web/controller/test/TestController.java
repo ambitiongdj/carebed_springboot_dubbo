92babaca6c616999bb52c480dfd7662f80c5159e
GDJ <916399802@qq.com>
2020-07-13 16:39:43 +0800

    修改代码测试提交

--- carebed-parent/carebed-web/manage-web/src/main/java/com/carebed/web/controller/test/TestController.java
+++ carebed-parent/carebed-web/manage-web/src/main/java/com/carebed/web/controller/test/TestController.java
@@ -4,11 +4,13 @@ import org.springframework.stereotype.Controller;
 
 /**
  * @ClassName TestController
- * @Description TODO
+ * @Description TestController
  * @Author GDJ
  * @Date 2020/07/13
  * @Version 1.0
  */
 @Controller
 public class TestController {
+
+
 }
