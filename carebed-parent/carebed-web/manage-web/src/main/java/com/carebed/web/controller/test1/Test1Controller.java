68afdc4cc44d3c42f80d041e5b9b1881e2c47e31
GDJ <916399802@qq.com>
2020-07-13 17:06:21 +0800

    提交test1

---
+++ carebed-parent/carebed-web/manage-web/src/main/java/com/carebed/web/controller/test1/Test1Controller.java
@@ -0,0 +1,11 @@
+package com.carebed.web.controller.test1;
+
+/**
+ * @ClassName Test1Controller
+ * @Description TODO
+ * @Author GDJ
+ * @Date 2020/07/13
+ * @Version 1.0
+ */
+public class Test1Controller {
+}
