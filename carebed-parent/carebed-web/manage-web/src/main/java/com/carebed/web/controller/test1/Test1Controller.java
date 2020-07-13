ba496e61987c19c1588383ebc99532861fe639b0
GDJ <916399802@qq.com>
2020-07-13 17:10:06 +0800

    张三提交

--- carebed-parent/carebed-web/manage-web/src/main/java/com/carebed/web/controller/test1/Test1Controller.java
+++ carebed-parent/carebed-web/manage-web/src/main/java/com/carebed/web/controller/test1/Test1Controller.java
@@ -2,10 +2,24 @@ package com.carebed.web.controller.test1;
 
 /**
  * @ClassName Test1Controller
- * @Description TODO
+ * @Description Test1Controller
  * @Author GDJ
  * @Date 2020/07/13
  * @Version 1.0
  */
 public class Test1Controller {
+
+    public static void main(String[] args) {
+        int a = 0;
+        int ab = 0;
+        int abc = 0;
+        int aa = 0;
+        int av = 0;
+        int ac = 0;
+        int aw = 0;
+        int aas = 0;
+        int ae = 0;
+        int asw = 0;
+        int awq = 0;
+    }
 }
